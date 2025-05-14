3b7a247dbedcfd958724a41a4b39944e3e223cb4
James <78121730+itsnewtjam@users.noreply.github.com>
2025-05-14 12:27:51 -0400

    treesitter context

--- dotfiles/.config/nvim/lua/newtjam/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/newtjam/plugins/treesitter.lua
@@ -1,29 +1,38 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require('nvim-treesitter.install').update({ with_sync = true })()
-  end,
-  config = function()
-    local configs = require('nvim-treesitter.configs')
-    configs.setup({
-      ensure_installed = {
-        "lua",
-        "javascript",
-        "typescript",
-        "json",
-        "html",
-        "php",
-        "css",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = { "latex" },
-        additional_vim_regex_highlighting = false,
-      },
-      autotag = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require('nvim-treesitter.install').update({ with_sync = true })()
+    end,
+    config = function()
+      local configs = require('nvim-treesitter.configs')
+      configs.setup({
+        ensure_installed = {
+          "lua",
+          "javascript",
+          "typescript",
+          "json",
+          "html",
+          "php",
+          "css",
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = { "latex" },
+          additional_vim_regex_highlighting = false,
+        },
+        autotag = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    after = "nvim-treesitter",
+    config = function()
+      require('treesitter-context').setup({})
+    end,
+  }
 }
