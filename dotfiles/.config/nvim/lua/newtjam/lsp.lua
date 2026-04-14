2b86808c272acd11f1bce6cce0555dc54f7b987d
James <78121730+itsnewtjam@users.noreply.github.com>
2026-04-14 17:07:26 -0400

    diagnostics config

--- dotfiles/.config/nvim/lua/newtjam/lsp.lua
+++ dotfiles/.config/nvim/lua/newtjam/lsp.lua
@@ -84,7 +84,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_floatgoto_next() end)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
     vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count = -1}) end)
     vim.keymap.set("n", "]d", function() vim.diagnostic.jump({count = 1}) end)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
@@ -130,4 +130,21 @@ cmp.setup({
   }, {
     { name = 'buffer' },
   }),
+})
+
+vim.diagnostic.config({
+  virtual_text = true,
+  jump = {
+    on_jump = function()
+      vim.diagnostic.open_float()
+    end,
+  },
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = true,
+    header = "",
+    prefix = "",
+  }
 })
